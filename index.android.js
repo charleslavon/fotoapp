d4486f7fd966e1e539ae0b9215faed6c9445dd10
charles <charleslavong@gmail.com>
2016-06-15 17:34:58 +0200

    Finally found a way to access camera roll. It's display only right now. Built off FB's CameraRollExample

--- index.android.js
+++ index.android.js
@@ -8,38 +8,17 @@ import {
   View
 } from 'react-native';
 
-//var Images = require('./App/components/imageLoader/imageLoader.android.js');
+var Images = require('./App/components/ImageLoader/ImageLoader.js');
 //var Trash = require('./App/views/trashcan/trash.android.js');
 
-var MOCKED_IMAGE_DATA = [
-  {thumbnail: 'http://i.imgur.com/UePbdph.jpg'}
-];
 
 class FotoApp extends Component {
   render() {
-    var image = MOCKED_IMAGE_DATA[0];
-
     return (
-      <View style={styles.container}>
-        <Image source={{uri: image.thumbnail}}
-              style={styles.thumbnail}/>
-      </View>
+      <Images/>
     );
 
   }
 }
-
-var styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  thumbnail: {
-    width: 53,
-    height: 81,
-  },
-});
 
 AppRegistry.registerComponent('FotoApp', () => FotoApp);
